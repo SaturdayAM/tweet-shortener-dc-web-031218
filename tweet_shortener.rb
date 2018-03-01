c0e70e7e3e5b9b6371d1b92de46e27b7bd220aeb
Jerry Lee <jwl62@georgetown.edu>
2018-03-01 07:35:03 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,3 +13,20 @@ def dictionary
     "and" => "&"
   }
 end
+
+def word_substituter(str)
+  arr = str.split(" ")
+  arr_2 = []
+  dict = dictionary
+  keys = dict.keys
+  
+  arr.each do |word|
+    if keys.include? word
+      arr_2 << dict[word]
+    else
+      arr_2 << word
+    end
+  end
+
+  arr_2.join(" ")
+end
